be152ae00502742dcb827c0623ad771535237c81
Neil Alcoseba <neil@localhost>
2013-05-13 17:02:58 +0800

    Adding of property page, ant build.xml, and dependencies

---
+++ src/org/alcoseba/addjarplugin/properties/AddJarPropertyPage.java
@@ -0,0 +1,15 @@
+package org.alcoseba.addjarplugin.properties;
+
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
+import org.eclipse.ui.IWorkbenchPropertyPage;
+import org.eclipse.ui.dialogs.PropertyPage;
+
+public class AddJarPropertyPage extends PropertyPage implements IWorkbenchPropertyPage {
+
+	@Override
+	protected Control createContents(Composite arg0) {
+		return null;
+	}
+
+}
